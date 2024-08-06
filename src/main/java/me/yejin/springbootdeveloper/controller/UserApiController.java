71da27f2596fc844f854e69ec8613d36c76a283a
yejin <yejinoh58@gmail.com>
2024-08-06 13:53:32 +0900

    logout api

--- src/main/java/me/yejin/springbootdeveloper/controller/UserApiController.java
+++ src/main/java/me/yejin/springbootdeveloper/controller/UserApiController.java
@@ -1,9 +1,14 @@
 package me.yejin.springbootdeveloper.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.yejin.springbootdeveloper.dto.AddUserRequest;
 import me.yejin.springbootdeveloper.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -15,6 +20,14 @@ public class UserApiController {
     @PostMapping("/user")
     public String signup(AddUserRequest request) {
         userService.save(request);
+
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response,
+                SecurityContextHolder.getContext().getAuthentication()); //로그아웃 담당 핸들러 이용
 
         return "redirect:/login";
     }
